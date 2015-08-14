39ddf1a5c63eea847b3a3309396b3e8bab57100e
rbogle <rbogle@usgs.gov>
2015-08-14 13:50:25 -0700

    Plugin init file added

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :members_block do
+  name 'Members Block plugin'
+  author 'Rian Bogle'
+  description 'A plugin for managing the members block on project pages'
+  version '0.0.1'
+end
